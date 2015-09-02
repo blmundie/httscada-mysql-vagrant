9eae824923d9b060719aef39d33b6ca1eda97507
Bryan Mundie <blmundie@gmail.com>
2015-09-02 13:45:29 -0400

    updated version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Blmundie"
 maintainer_email "bryan.mundie@hightidetechnologies.com"
 license          "MIT"
 description      "Configure mysql on vagrant"
-version          "0.7"
+version          "0.8"
 
 recipe 'httscada-mysql-vagrant::mysql_setup', 'Set mysql'
 depends 'mysql', '~> 6.1'
