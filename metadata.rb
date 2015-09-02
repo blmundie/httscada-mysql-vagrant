0361d56ce6dc18a957c36eec622fc0fa6cfd1817
Bryan Mundie <blmundie@gmail.com>
2015-09-02 13:05:43 -0400

    remove extra code

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Blmundie"
 maintainer_email "bryan.mundie@hightidetechnologies.com"
 license          "MIT"
 description      "Configure mysql on vagrant"
-version          "0.55"
+version          "0.6"
 
 recipe 'httscada-mysql-vagrant::mysql_setup', 'Set mysql'
 depends 'mysql', '~> 6.0'
--- recipes/mysql_setup.rb
+++ recipes/mysql_setup.rb
@@ -2,7 +2,6 @@ mysql_service 'default' do
   port '3306'
   version '5.5'
   initial_root_password 'admin'
-  provider Chef::Provider::MysqlService::Sysvinit
   action [:create, :start]
 end
 
