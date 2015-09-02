bedfc88ede43885bab6109de6efcecb550cdfb04
Bryan Mundie <blmundie@gmail.com>
2015-09-02 13:51:21 -0400

    updated to new mysql version

--- recipes/mysql_setup.rb
+++ recipes/mysql_setup.rb
@@ -1,8 +1,7 @@
 mysql_service 'default' do
   port '3306'
-  version '5.5'
+  version '5.7'
   initial_root_password 'admin'
-  provider Chef::Provider::MysqlService::Sysvinit
   action [:create, :start]
 end
 
